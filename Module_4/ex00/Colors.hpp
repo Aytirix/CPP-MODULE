dbc65aa7a8d4d4331c19d692aede2051fe9fe4a3
Théo <aytirix@hotmail.com>
2024-12-16 18:24:34 +0100

    debug cpp 04

---
+++ Module_4/ex00/Colors.hpp
@@ -0,0 +1,21 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+#define RESET_TERM "\033[0m"
+#define RESET "\033[0m"
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
+#define BOLD "\033[1m" // Gras
+#define UNDERLINE "\033[4m" // Souligné
+#define BLINK "\033[5m" // Clignotant
+#define REVERSE "\033[7m" // Surligné
+#define CONCEALED "\033[8m" // Invisible
+
+#endif
--- Module_4/ex00/main.cpp
+++ Module_4/ex00/main.cpp
@@ -3,32 +3,38 @@
 #include "Dog.hpp"
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
-#include <map>
-
-void	printColor(const std::string &text, std::string color)
-{
-	std::map<std::string, std::string> colorMap;
-	colorMap["black"] = "30";
-	colorMap["red"] = "31";
-	colorMap["green"] = "32";
-	colorMap["yellow"] = "33";
-	colorMap["blue"] = "34";
-	colorMap["magenta"] = "35";
-	colorMap["cyan"] = "36";
-	colorMap["white"] = "37";
-	std::map<std::string, std::string>::iterator it = colorMap.find(color);
-	if (it != colorMap.end())
-		color = it->second;
-	else
-		color = "37";
-	std::cout << "\033[1;" << color << "m" << text << "\033[0m" << std::endl;
-}
+#include "Colors.hpp"
 
 int	main(void)
 {
 	system("clear");
+
+	Animal *animal[10];
+
+	std::cout << GREEN REVERSE "Creating 5 dogs" RESET << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		animal[i] = new Dog();
+		animal[i]->makeSound();
+		std::cout << std::endl;
+	}
+	std::cout << GREEN REVERSE "\nCreating 5 cats" RESET << std::endl;
+	for (int i = 5; i < 10; i++)
+	{
+		animal[i] = new Cat();
+		animal[i]->makeSound();
+		std::cout << std::endl;
+	}
+
+	std::cout << RED REVERSE "\nDeleting all animals" RESET << std::endl;
+	for (int i = 0; i < 10; i++)
+	{
+		delete animal[i];
+		std::cout << std::endl;
+	}
+
 	// Test 1: Animal
-	printColor("Test 1: Animal", "yellow");
+	std::cout << YELLOW REVERSE "\nTest 1: Animal" RESET << std::endl;
 	const Animal *meta = new Animal();
 	const Animal *meta2 = new Animal(*meta);
 	std::cout << "Type: " << meta->getType() << std::endl;
@@ -38,28 +44,28 @@ int	main(void)
 	delete meta2;
 
 	// Test 2: Dog
-	printColor("\nTest 2: Dog", "green");
+	std::cout << GREEN REVERSE "\nTest 2: Dog" RESET << std::endl;
 	const Animal *dog = new Dog();
 	std::cout << "Type: " << dog->getType() << std::endl;
 	dog->makeSound();
 	delete dog;
 
 	// Test 3: Cat
-	printColor("\nTest 3: Cat", "blue");
+	std::cout << BLUE REVERSE "\nTest 3: Cat" RESET << std::endl;
 	const Animal *cat = new Cat();
 	std::cout << "Type: " << cat->getType() << std::endl;
 	cat->makeSound();
 	delete cat;
 
 	// Test 4: WrongAnimal
-	printColor("\nTest 4: WrongAnimal", "magenta");
+	std::cout << MAGENTA REVERSE "\nTest 4: WrongAnimal" RESET << std::endl;
 	const WrongAnimal *wrongAnimal = new WrongAnimal();
 	std::cout << "Type: " << wrongAnimal->getType() << std::endl;
 	wrongAnimal->makeSound();
 	delete wrongAnimal;
 
 	// Test 5: WrongCat
-	printColor("\nTest 5: WrongCat", "cyan");
+	std::cout << CYAN REVERSE "\nTest 5: WrongCat" RESET << std::endl;
 	const WrongCat *wrongCat = new WrongCat();
 	std::cout << "Type: " << wrongCat->getType() << std::endl;
 	wrongCat->makeSound();
